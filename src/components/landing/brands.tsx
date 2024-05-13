561ef8ef70455c723662b434ac55cd753fe26f90
iamjuangonzalez <juanjosegd96@gmail.com>
2024-05-13 12:38:31 -0500

    add changes of services and brands

--- src/components/landing/brands.tsx
+++ src/components/landing/brands.tsx
@@ -3,21 +3,23 @@ import style from "@/styles/landing/Brands.module.scss";
 
 function Brands() {
   return (
-    <section className={`${style.brands} container`}>
-      <div className={style.brand__logo}>
-        <img src="/icons/brand.png" alt="brand1" />
-      </div>
-      <div className={style.brand__logo}>
-        <img src="/icons/brand.png" alt="brand2" />
-      </div>
-      <div className={style.brand__logo}>
-        <img src="/icons/brand.png" alt="brand3" />
-      </div>
-      <div className={style.brand__logo}>
-        <img src="/icons/brand.png" alt="brand4" />
-      </div>
-      <div className={style.brand__logo}>
-        <img src="/icons/brand.png" alt="brand5" />
+    <section className="container">
+      <div className={style.brands}>
+        <div className={style.brand__logo}>
+          <img src="/icons/brand.png" alt="brand1" />
+        </div>
+        <div className={style.brand__logo}>
+          <img src="/icons/brand.png" alt="brand2" />
+        </div>
+        <div className={style.brand__logo}>
+          <img src="/icons/brand.png" alt="brand3" />
+        </div>
+        <div className={style.brand__logo}>
+          <img src="/icons/brand.png" alt="brand4" />
+        </div>
+        <div className={style.brand__logo}>
+          <img src="/icons/brand.png" alt="brand5" />
+        </div>
       </div>
     </section>
   );
--- src/components/landing/services.tsx
+++ src/components/landing/services.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+
+import style from "@/styles/landing/Services.module.scss";
 
 function Services() {
   return (
-    <div>services</div>
-  )
+    <section className={style.services}>
+      <div className="container">
+        <div className={style.contentHeader}>
+          <small className={style.titleHeader}>Our Services</small>
+          <h2 className={style.title}>
+            We provide useful solution for your business
+          </h2>
+          <p className={style.description}>
+            We are committed to providing our customers with exceptional service
+            while offering our employees the best training.
+          </p>
+        </div>
+        <div className={style.contentBody}>
+          <div className={style.card}>
+            <h1>Strategic Business Planning</h1>
+            <p>
+              Lorem ipsum quis auctor elit sed vulputate mi sit amet mauris
+              commodo quis imperdiet massa tincidunt nunc
+            </p>
+            <a href="#">Why choose us →</a>
+          </div>
+          <div className={`${style.card}`}>
+            <h1>Strategic Business Planning</h1>
+            <p>
+              Lorem ipsum quis auctor elit sed vulputate mi sit amet mauris
+              commodo quis imperdiet massa tincidunt nunc
+            </p>
+            <a href="#">Why choose us →</a>
+          </div>
+          <div className={style.card}>
+            <h1>Strategic Business Planning</h1>
+            <p>
+              Lorem ipsum quis auctor elit sed vulputate mi sit amet mauris
+              commodo quis imperdiet massa tincidunt nunc
+            </p>
+            <a href="#">Why choose us →</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
