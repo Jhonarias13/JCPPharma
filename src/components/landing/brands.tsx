7b6ee08c27c29aec63072e473d47c4ea81ebda2b
iamjuangonzalez <juanjosegd96@gmail.com>
2024-05-13 12:01:04 -0500

    add section brands

--- src/components/landing/brands.tsx
+++ src/components/landing/brands.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import style from "@/styles/landing/Brands.module.scss";
 
 function Brands() {
   return (
-    <div>brands</div>
-  )
+    <section className={`${style.brands} container`}>
+      <div className={style.brand__logo}>
+        <img src="/icons/brand.png" alt="brand1" />
+      </div>
+      <div className={style.brand__logo}>
+        <img src="/icons/brand.png" alt="brand2" />
+      </div>
+      <div className={style.brand__logo}>
+        <img src="/icons/brand.png" alt="brand3" />
+      </div>
+      <div className={style.brand__logo}>
+        <img src="/icons/brand.png" alt="brand4" />
+      </div>
+      <div className={style.brand__logo}>
+        <img src="/icons/brand.png" alt="brand5" />
+      </div>
+    </section>
+  );
 }
 
-export default Brands
+export default Brands;
